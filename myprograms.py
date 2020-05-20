892e2928c367b62dd45c9311c3a2ec3f232c1163
abrahamanderson19972020 <62431792+abrahamanderson19972020@users.noreply.github.com>
2020-05-21 01:07:43 +0200

    Update myprograms.py

--- myprograms.py
+++ myprograms.py
@@ -1,3 +1,4 @@
+#This a program that finds age
 def age_finder():
     age= int(input("PLease enter your guess :"))
     while True:
@@ -18,41 +19,3 @@ print(age_finder())
 
 
 
-""" Christmas Tree """
-picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
-]
-for item in picture:
-    for i in item:
-        if i==0:
-            
-            print(" ",end=" ")
-        else:
-            print("*",end=" ")
-    print("")
-
-""" The end of the program of Christmas tree"""
-
-#Guessing game:
-import random
-
-while True:
-    
-    try:
-        number=random.randint(0, 10)
-        guess = int(input("Please enter your guess between 1-9 :"))
-        if 0 < guess < 10: 
-            if number == guess:
-                print("Welldone. Your guess is right")
-                break
-            else:
-                print(f"The aswer was {number}")
-        else:
-            print("Your guess should be between 0 and 9!")
-    except ValueError as err:
-        print(err)
